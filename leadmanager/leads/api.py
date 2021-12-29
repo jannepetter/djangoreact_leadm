a221de22b87955d8a978d0d9aa497ed3f880d3e4
jannepetter <peterdev@gmail.com>
2021-12-29 13:59:15 +0200

    django setup rdy

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,10 @@
+from rest_framework.decorators import permission_classes
+from .models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LeadSerializer
